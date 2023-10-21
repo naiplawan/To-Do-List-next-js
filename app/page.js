a7903e5de3a3651cc5ce81c137acd860894c9557
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-10-21 23:09:24 +0700

    feat:style page with antdesign

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import { useState,useEffect} from "react"
-import { Checkbox } from 'antd';
+import { Checkbox,Button} from 'antd';
+import { DeleteOutlined } from "@ant-design/icons";
 
 
 export default function Page() {
@@ -42,10 +43,11 @@ export default function Page() {
 
   return (
     <>
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <h1 className="text-5xl text-center font-sans m-5">To-do-list</h1>
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-orange-200 via-amber-100 to-yellow-300">
+        <div className="flex flex-col justify-center items-center bg-orange-300 rounded-md p-10 "> 
+        <h1 className="text-5xl text-white text-center font-sans m-5">To-do-list 📝</h1>
         <input
-          className="text-center border-2 border-black rounded-md h-10 w-100 text-black"
+          className="text-center rounded-full h-10 w-100 text-slate-800 "
           type="text"
           placeholder="Enter your task"
           onKeyDown={handleKeyDown}
@@ -53,24 +55,27 @@ export default function Page() {
         <br />
         <p>Here is a list of things to do:</p>
         <br />
-        <ul className="flex flex-col justify-center item-between">
+        <ul className="flex flex-col justify-center item-between ">
           {Array.isArray(todos) &&
             todos.map((todo, index) => (
-              <li key={index} className={`flex flex-row h-10 justify-between items-center text-red-800 text-center m-2 px-3 bottom-3 border-2 border-black rounded-md bg-white ${
+              <li key={index} className={`flex flex-row h-10 justify-between items-center text-orange-900 text-center m-1 px-3 bottom-3  rounded-md bg-white ${
                 todo.completed ? 'bg-green-200' : ''
               }`}>
                  <Checkbox
-                 className="mr-2"
+                 className="mr-4"
                   checked={todo.completed}
                   onChange={(event) => handleCheckboxChange(event, index)}
                 />
                 {todo.text}
-                <button className="text-red-500 px-3" onClick={() => handleDeleteToDo(index)}>
-                  x
-                </button>
+                <Button className="text-orange-700 px-3"
+                type="dash" 
+                icon= {<DeleteOutlined />}
+                 onClick={() => handleDeleteToDo(index)}>
+                </Button>
               </li>
             ))}
         </ul>
+      </div>
       </div>
     </>
   );
